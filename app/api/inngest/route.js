ad30c942d8c0538974ffdfc298e0b5ff42379e66
Venkata-Giridhar-Garikipati <venkatagiridhargarikipati@gmail.com>
2025-01-25 18:34:47 +0530

    first commit

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,10 +1,13 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
 import {  GenerateNotes, GenerateStudyTypeContent, helloWorld } from "@/inngest/functions";
+export const runtime = 'edge';
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
+  streaming:'allow',
+
   functions: [
     /* your functions will be passed here later! */
     helloWorld,
